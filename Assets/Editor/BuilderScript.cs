d0971bd0906e933b3c667ed83c3f8cd86df0bcc5
devesh-ls <devesh.beniwal@logicsimplified.com>
2021-10-14 10:50:17 +0530

    Initial commit

---
+++ Assets/Editor/BuilderScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+class WebGLBuilder
+{
+    static void build()
+    {
+
+        // Place all your scenes here
+        string[] scenes = {"Assets/Scenes/Demo.unity"};
+
+        //string pathToDeploy = "D:/PROJECTS/BuildTest/WINDOWS_BUILD";
+
+        BuildPlayerOptions ops = new BuildPlayerOptions();
+        ops.scenes = scenes;
+        ops.target = BuildTarget.WebGL;
+        ops.locationPathName = "D:/PROJECTS/BuildTest/WEBGL_BUILD/v1";
+
+        BuildPipeline.BuildPlayer(ops);
+    }
+}
